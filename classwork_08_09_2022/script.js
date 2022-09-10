f15a0262706f9a473b6da87a22244766e85e4a23
Solo Leveling <SoloLevelingMansur2005@gmail.com>
2022-09-10 19:40:05 +0600

    HW_10_09_2022

---
+++ classwork_08_09_2022/script.js
@@ -0,0 +1,54 @@
+// one
+// let one = +prompt("Введите первое число")
+// let two = +prompt("Введите второе число")
+// if (one>two) {
+//     alert(two)
+// } else {
+//     alert(one)
+// }
+
+
+
+
+// two
+// let one = +prompt("Введите число")
+// let two = +prompt("Введите степень")
+// alert(one**two)
+
+
+
+
+// three
+// let one = +prompt("Введите число")
+// let two = +prompt("Введите число")
+// let znak = prompt("Введите знак")
+// if (znak == "+") {
+//     alert(one + two)
+// } else if (znak == "-") {
+//     alert(one - two)
+// } else if (znak == "*") {
+//     alert(one * two)
+// } else if (znak == "/") {
+//     alert(one / two)
+// }
+
+
+
+// four
+// function calc(num) {
+//     for(let i=2; i<num; i++) {
+//         if (num%i==0) {
+//             return false
+//         } else {
+//             return true
+//         }
+//     }
+// }
+
+// alert(calc())
+
+
+
+
+
+// five
